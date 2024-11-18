8e98667f3a8e5557ca5750a9ee697ca8648e5a68
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-11-18 14:21:51 +0200

    feat: build default template

--- frontend/src/templates/DefaultTemplate.vue
+++ frontend/src/templates/DefaultTemplate.vue
@@ -1,14 +1,62 @@
 <template>
-  <div>
-    <header>Header</header>
-    <aside>Sidebar</aside>
-    <footer>Footer</footer>
-    <main>
+  <div class="wrapper" :style="`padding-left:${sidebarWidth}px`">
+    <LayoutHeader class="header" />
+    <layout-sidebar class="sidebar" ref="sidebarRef" />
+    <layout-footer class="footer" :style="`left:${sidebarWidth}px`"/>
+    <main class="page">
       <slot />
     </main>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LayoutHeader from '@/components/layout/LayoutHeader.vue'
+import LayoutSidebar from '@/components/layout/LayoutSidebar.vue'
+import LayoutFooter from '@/components/layout/LayoutFooter.vue'
 
-<style scoped></style>
+import { onMounted, ref } from 'vue'
+
+const sidebarRef = ref<InstanceType<typeof LayoutSidebar> | null>(null)
+const sidebarWidth = ref(0)
+
+function calcSidebarWidth() {
+  if (!sidebarRef.value) return
+
+  sidebarWidth.value = sidebarRef.value.$el.clientWidth
+}
+
+onMounted(() => {
+  calcSidebarWidth()
+})
+</script>
+
+<style scoped>
+.wrapper {
+  min-height: 100svh;
+  padding-top: 128px;
+  padding-bottom: 128px;
+}
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+}
+.sidebar {
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 90;
+}
+.page {
+  padding: 0 100px;
+}
+.footer {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  z-index: 80;
+}
+</style>
