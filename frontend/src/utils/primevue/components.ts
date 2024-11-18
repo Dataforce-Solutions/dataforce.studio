e8c6d9b007b2d1d27aa6998c1ea763d9f4055064
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-11-18 11:26:36 +0200

    feat: add PrimeVue Button, Card

--- frontend/src/utils/primevue/components.ts
+++ frontend/src/utils/primevue/components.ts
@@ -1,4 +1,7 @@
 import type { App } from 'vue'
+import { Button, Card } from 'primevue'
 
 export const addComponents = (app: App) => {
+  app.component('DButton', Button)
+  app.component('DCard', Card)
 }
