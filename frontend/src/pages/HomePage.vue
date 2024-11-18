58fb7965f27db9dd9363e9528e7ba4043386d102
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-11-18 14:23:47 +0200

    feat: build home page

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>Home page</div>
+  <div>
+    <div class="headings">
+      <h1 class="title">Pick a machine learning task</h1>
+      <p class="text">
+        Choose from a variety of tasks that best fit your needs, or explore them all to discover new
+        possibilities.
+      </p>
+    </div>
+    <div class="body">
+      <available-tasks />
+      <hr size="1" color="#DCE4EF">
+      <not-available-tasks />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AvailableTasks from '@/components/tasks/AvailableTasks.vue'
+import NotAvailableTasks from '@/components/tasks/NotAvailableTasks.vue'
+</script>
 
-<style scoped></style>
+<style scoped>
+.body {
+  display: flex;
+  flex-direction: column;
+  gap: 36px;
+}
+.headings {
+  margin-bottom: 56px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+</style>
