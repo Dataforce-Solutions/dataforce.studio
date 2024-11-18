52764f479c4d438cb3d765c478af930afdd7b9d2
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-11-18 14:19:55 +0200

    feat: add footer component

---
+++ frontend/src/components/layout/LayoutFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <p>© DataForce Solutions GmbH 2024</p>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.footer {
+  padding: 10px;
+  background-color: var(--color-background);
+  border-top: 1px solid var(--color-border);
+  text-align: center;
+  line-height: 2;
+}
+</style>
