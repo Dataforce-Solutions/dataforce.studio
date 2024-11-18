f733aa0cf3a37391228cab409b8faa675c0ab065
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-11-18 16:24:35 +0200

    feat: add from components

--- frontend/src/utils/primevue/components.ts
+++ frontend/src/utils/primevue/components.ts
@@ -1,7 +1,11 @@
 import type { App } from 'vue'
-import { Button, Card } from 'primevue'
+import { Button, Card, InputText, Message } from 'primevue'
+import { Form } from '@primevue/forms'
 
 export const addComponents = (app: App) => {
   app.component('DButton', Button)
   app.component('DCard', Card)
+  app.component('DInputText', InputText)
+  app.component('DMessage', Message)
+  app.component('DForm', Form)
 }
