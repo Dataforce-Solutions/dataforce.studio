4d33e2220d331d4d70708049564113f492590d22
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-11-20 12:54:21 +0200

    feat: add dataforce api interceptors

---
+++ frontend/src/utils/api/DataforceApi.interceptors.ts
@@ -0,0 +1,67 @@
+import type { AxiosInstance, AxiosResponse } from 'axios'
+import type { IPostRefreshTokenRequest, IPostRefreshTokenResponse } from './DataforceApi.interfaces'
+
+export const installDataforceInterceptors = (api: AxiosInstance) => {
+  api.interceptors.request.use(
+    (config) => {
+      if (config.skipInterceptors) {
+        return config
+      }
+      const token = localStorage.getItem('token')
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
+      return config
+    },
+    (error) => {
+      return Promise.reject(error)
+    },
+  )
+
+  api.interceptors.response.use(
+    (response) => {
+      return response
+    },
+    async (error) => {
+      if (error.response && error.response.status === 401) {
+        try {
+          const refresh_token = localStorage.getItem('refreshToken')
+
+          if (!refresh_token) throw new Error('Refresh token is not exist')
+
+          const data: IPostRefreshTokenRequest = { refresh_token }
+
+          const { data: responseData } = await api.post<IPostRefreshTokenResponse>(
+            '/auth/refresh',
+            data,
+          )
+
+          const newToken = responseData.access_token
+
+          localStorage.setItem('token', newToken)
+
+          error.config.headers.Authorization = `Bearer ${newToken}`
+          return api.request(error.config)
+        } catch (refreshError) {
+          console.error('Failed to refresh token:', refreshError)
+        }
+      }
+
+      return Promise.reject(error)
+    },
+  )
+
+  api.interceptors.request.use((config) => {
+    if (config.data && typeof config.data === 'object' && !(config.data instanceof FormData)) {
+      const formData = new FormData()
+      for (const key in config.data) {
+        if (Object.prototype.hasOwnProperty.call(config.data, key)) {
+          formData.append(key, config.data[key])
+        }
+      }
+      config.data = formData
+      config.headers['Content-Type'] = 'multipart/form-data'
+    }
+    return config
+  })
+}
