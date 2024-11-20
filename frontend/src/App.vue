d2d38e07a256cd65146e59925748bce4dfda1372
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-11-20 14:20:12 +0200

    feat: add logged in check while site opening

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,6 +7,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import AppTemplate from './templates/AppTemplate.vue'
+import { onBeforeMount } from 'vue'
+
+import { useAuthStore } from './stores/auth'
+
+const authStore = useAuthStore()
+
+onBeforeMount(() => {
+  authStore.checkIsLoggedId()
+})
 </script>
 
 <style scoped></style>
