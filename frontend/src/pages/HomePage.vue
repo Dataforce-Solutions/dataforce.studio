c3f9a11de697ae38ceeb819aff7ae3d9dd6bc032
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-11-24 22:53:18 +0200

    style: home page view

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <div class="headings">
-      <h1 class="title">Pick a machine learning task</h1>
-      <p class="text">
+      <h1 class="main-title">Pick a machine learning task</h1>
+      <p class="sub-title">
         Choose from a variety of tasks that best fit your needs, or explore them all to discover new
         possibilities.
       </p>
     </div>
     <div class="body">
       <available-tasks />
-      <hr size="1" color="#DCE4EF">
+      <div class="divider"></div>
       <not-available-tasks />
     </div>
   </div>
@@ -27,7 +27,7 @@ import NotAvailableTasks from '@/components/tasks/NotAvailableTasks.vue'
   gap: 36px;
 }
 .headings {
-  margin-bottom: 56px;
+  margin-bottom: 44px;
   display: flex;
   flex-direction: column;
   gap: 24px;
