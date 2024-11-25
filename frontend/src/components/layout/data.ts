0b8b80c342ca9067a44b4f625099eee752caff88
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-11-25 23:50:56 +0200

    feat: move sidebar menu data

---
+++ frontend/src/components/layout/data.ts
@@ -0,0 +1,22 @@
+import { Rabbit, Timer, Frame } from 'lucide-vue-next'
+
+export const sidebarMenu = [
+  {
+    id: 1,
+    label: 'Express task',
+    icon: Rabbit,
+    route: 'home',
+  },
+  {
+    id: 2,
+    label: 'Run time',
+    icon: Timer,
+    route: 'sign-up',
+  },
+  {
+    id: 3,
+    label: 'Projects',
+    icon: Frame,
+    route: 'sign-up',
+  },
+]
