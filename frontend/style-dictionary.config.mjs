a04f08810000b07924556d5f90b76fbe49e2424b
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-11-27 00:21:09 +0200

    feat: add style dictionary config

---
+++ frontend/style-dictionary.config.mjs
@@ -0,0 +1,74 @@
+import StyleDictionary from 'style-dictionary'
+
+StyleDictionary.registerTransform({
+  name: 'name/cti/kebab-prefixed',
+  type: 'name',
+  transform: (token) => `p-${token.path.join('-')}`,
+})
+
+StyleDictionary.registerTransform({
+  name: 'value/px',
+  type: 'value',
+  filter: (token) => token['$type'] === 'number',
+  transform: (token) => `${token['$value']}px`,
+})
+
+StyleDictionary.registerTransform({
+  name: 'value/boxShadow',
+  type: 'value',
+  filter: (token) => token['$type'] === 'boxShadow',
+  transform: (token) => {
+    console.warn(token)
+    const shadow = token.original['$value'];
+    if (Array.isArray(shadow)) {
+      const shadowValues = shadow.map(s => {
+        const { color, x, y, blur, spread } = s;
+        return `${x}px ${y}px ${blur}px ${spread}px ${color}`;
+      })
+      return shadowValues.join(', ')
+    } else {
+      const { color, x, y, blur, spread } = shadow;
+      return `${x}px ${y}px ${blur}px ${spread}px ${color}`;
+    }
+  }
+});
+
+StyleDictionary.registerTransformGroup({
+  name: 'custom/css',
+  transforms: ['name/cti/kebab-prefixed', 'value/px', 'value/boxShadow', 'color/hex'],
+})
+
+export default {
+  // source: ['tokens/tokens-styles-light.json',], // for light
+  source: ['tokens/tokens-styles-dark.json'], // for dark
+  platforms: {
+    light: {
+      transformGroup: 'custom/css',
+      buildPath: 'src/assets/theme/',
+      files: [
+        {
+          destination: 'light-theme.css',
+          format: 'css/variables',
+          options: {
+            selector: '#app',
+          },
+          filter: (token) => token.filePath.includes('light'),
+        },
+      ],
+    },
+    dark: {
+      transformGroup: 'custom/css',
+      buildPath: 'src/assets/theme/',
+      files: [
+        {
+          destination: 'dark-theme.css',
+          format: 'css/variables',
+          options: {
+            selector: "[data-theme='dark'] #app",
+          },
+          filter: (token) => token.filePath.includes('dark'),
+        },
+      ],
+    },
+  },
+}
