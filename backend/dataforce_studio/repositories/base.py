eba5c1c8021a3b113070cd0bedab1615f7e96455
Nikita Gonchar <nkt2397@gmail.com>
2025-04-21 23:28:29 +0200

    chore: inject engine to repos

--- backend/dataforce_studio/repositories/base.py
+++ backend/dataforce_studio/repositories/base.py
@@ -1,20 +1,9 @@
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
-
-from dataforce_studio.settings import config
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 
 
 class RepositoryBase:
-    _engine: AsyncEngine | None = None
-
-    @classmethod
-    def get_engine(cls: "RepositoryBase") -> AsyncEngine:
-        if cls._engine is None:
-            cls._engine = cls._create_engine()
-        return cls._engine
-
-    @staticmethod
-    def _create_engine() -> AsyncEngine:
-        return create_async_engine(config.POSTGRESQL_DSN)
+    def __init__(self, engine: AsyncEngine) -> None:
+        self._engine = engine
 
     def _get_session(self) -> AsyncSession:
-        return AsyncSession(self.get_engine(), expire_on_commit=True)
+        return AsyncSession(self._engine, expire_on_commit=True)
